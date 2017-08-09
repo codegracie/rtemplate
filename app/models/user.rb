9ef4fadb5efbb67d203c2389038ce149ac2a26c8
gracesojor <grace.sojor@gmail.com>
2017-08-09 10:02:06 +0000

    Activate account method

--- app/models/user.rb
+++ app/models/user.rb
@@ -127,6 +127,12 @@ class User < ActiveRecord::Base
   # end
 
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
